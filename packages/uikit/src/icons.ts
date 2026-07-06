cf80bd4690a9179bfa3cf1390bd423f76b15662c
Vedant Chainani <vedantchainani1084@gmail.com>
2026-07-06 20:47:24 +0530

    feat(packages): alias hugeicons renderer

--- packages/uikit/src/icons.ts
+++ packages/uikit/src/icons.ts
@@ -1,4 +1,5 @@
 export { HugeiconsIcon as HugeiconsIconData } from "@hugeicons/core-free-icons";
 export * from "@hugeicons/core-free-icons";
 export * from "@hugeicons/react";
-export { HugeiconsIcon } from "@hugeicons/react";
+export { HugeiconsIcon, HugeiconsIcon as Icon } from "@hugeicons/react";
+export type { HugeiconsIconProps as IconProps } from "@hugeicons/react";
