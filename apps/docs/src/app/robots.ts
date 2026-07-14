1546afce89162b3a2d548e5c44074106851f1e0d
Vedant Chainani <vedantchainani1084@gmail.com>
2026-07-14 20:33:23 +0530

    refactor(docs): remove gallery capture tooling

--- apps/docs/src/app/robots.ts
+++ apps/docs/src/app/robots.ts
@@ -7,7 +7,7 @@ export default function robots(): MetadataRoute.Robots {
     host: site.url,
     rules: {
       allow: "/",
-      disallow: ["/api/", "/component-gallery-capture"],
+      disallow: ["/api/"],
       userAgent: "*",
     },
     sitemap: absoluteSiteUrl("/sitemap.xml"),
