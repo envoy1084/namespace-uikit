4f889e4adf1b4ef3f2dfc93c063d47c936016334
Vedant Chainani <vedantchainani1084@gmail.com>
2026-07-27 01:24:32 +0530

    feat(packages): add resumable registration attempts

--- packages/ens-components/src/hooks/index.ts
+++ packages/ens-components/src/hooks/index.ts
@@ -2,4 +2,5 @@ export * from "#/hooks/use-commitments";
 export * from "#/hooks/use-commitment-status";
 export * from "#/hooks/use-name-availability";
 export * from "#/hooks/use-name-price";
+export * from "#/hooks/use-registration-attempts";
 export * from "#/hooks/use-registration-payment-status";
