95a17c8aede4f5898a3e9512486b8d0f570fb315
Vedant Chainani <vedantchainani1084@gmail.com>
2026-07-28 20:22:33 +0530

    feat(packages): expand profile address presets

---
+++ packages/ens-components/src/components/icons/chain/fantom.tsx
@@ -0,0 +1,26 @@
+import type { SVGProps } from "react";
+export const ChainFantomIcon = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="1em"
+    height="1em"
+    fill="none"
+    viewBox="0 0 32 32"
+    {...props}
+  >
+    <path
+      fill="#26B6EA"
+      d="M32 16c0-8.837-7.163-16-16-16S0 7.163 0 16s7.163 16 16 16 16-7.163 16-16"
+    />
+    <path
+      fill="#fff"
+      fillRule="evenodd"
+      d="m15.97 7-5.615 3.186V22.54l5.615 3.282 5.68-3.28V10.185zm.033.941-4.238 2.541L16 12.952l4.235-2.47zm-4.709 3.294v4.236l3.765-2.109zm5.177 3.06v4.234l3.764-2.108zm4.235-3.06v4.236l-3.765-2.109zm-5.176 3.057v4.237l-3.765-2.109zm-4.236 2.855v4.912L16 24.88l4.707-2.823v-4.913l-4.706 2.562z"
+      clipRule="evenodd"
+    />
+    <path
+      fill="#fff"
+      d="M8 23.56v-2.913h.941v2.447l2.353 1.214v1.045zM24 9.264v2.913h-.941V9.729l-2.353-1.211V7.47z"
+    />
+  </svg>
+);
---
+++ packages/ens-components/src/components/icons/chain/monero.tsx
@@ -0,0 +1,20 @@
+import type { SVGProps } from "react";
+export const ChainMoneroIcon = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="1em"
+    height="1em"
+    fill="none"
+    viewBox="0 0 32 32"
+    {...props}
+  >
+    <path
+      fill="#F60"
+      d="M32 16c0-8.837-7.163-16-16-16S0 7.163 0 16s7.163 16 16 16 16-7.163 16-16"
+    />
+    <path
+      fill="#fff"
+      d="M16 5.333a10.653 10.653 0 0 1 10.124 14h-3.457v-8.918L15.99 17.37l-6.658-6.947v8.91H5.875a10.5 10.5 0 0 1-.542-3.325A10.666 10.666 0 0 1 16 5.333m.03 15.196 4.637-4.637v5.441h4.442c-1.972 3.255-5.253 5.335-9.11 5.334-3.844 0-7.226-2.266-9.108-5.334h4.442v-5.441z"
+    />
+  </svg>
+);
